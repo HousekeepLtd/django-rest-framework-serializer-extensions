e4079791a99e975ee1bea877c5cdddc3801e6e3f
Tiago Santos <tiago.santos@housekeep.com>
2022-07-05 10:56:16 +0100

    Drop python 3.6 support

--- setup.py
+++ setup.py
@@ -126,7 +126,6 @@ setup(
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
